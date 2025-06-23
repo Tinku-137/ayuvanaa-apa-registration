7fa936087ca8324a1ecbaa20227eef8ad71c708d
praveen kumar savaram <75710253+Tinku-137@users.noreply.github.com>
2025-06-23 14:01:17 +0530

    Update gsheet.py

--- gsheet.py
+++ gsheet.py
@@ -1,16 +1,22 @@
 import gspread
+import json
+import os
 from google.oauth2 import service_account
+from dotenv import load_dotenv
 
-# Load credentials from secrets.json file
-creds = service_account.Credentials.from_service_account_file("secrets.json")
+load_dotenv()
 
-# Authenticate gspread with those credentials
+SHEET_NAME = "Ayuvanaa Registrations"
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+service_account_info = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+creds = service_account.Credentials.from_service_account_info(service_account_info)
 client = gspread.authorize(creds)
-
-# Open the Google Sheet
-SHEET_NAME = "Ayuvanaa Registrations"  # make sure this matches your actual sheet name
 sheet = client.open(SHEET_NAME).sheet1
 
-def append_to_sheet(data: dict):
-    values = list(data.values())
-    sheet.append_row(values)
+def append_to_sheet(row_data):
+    sheet.append_row(row_data)
